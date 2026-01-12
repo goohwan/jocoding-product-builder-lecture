d7996f57d9f7ad8bf07f2d0fdcf59bdb31d558c6
goohwan <goohwan@gmail.com>
2026-01-12 11:20:46 +0000

    chore(firebase): update config with user provided credentials

--- firebase.js
+++ firebase.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyAHFjXbhAgcMCqS-wseY3TXyQbF_xDLgoU",
+  authDomain: "food-db-9a00a.firebaseapp.com",
+  projectId: "food-db-9a00a",
+  storageBucket: "food-db-9a00a.appspot.com",
+  messagingSenderId: "253957620368",
+  appId: "1:253957620368:web:8db835b6ae7d2bf2c2ce15",
+  measurementId: "G-L6LZP9K1SF"
 };
 
 // Initialize Firebase
